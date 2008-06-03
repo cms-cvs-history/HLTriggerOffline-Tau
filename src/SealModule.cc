04d196024592025c094353058b2db16dc97b332e
cmsbuild <>
2008-06-03 21:30:48 +0000

    This commit was manufactured by cvs2git to create tag 'HLTriggerOffline- Tau-V02-01-11'.
    Sprout from master 2008-04-28 23:18:34 UTC Chi Nhan Nguyen <sha1-d7c3810b3b8595346f468e2d6413da596572dbe0@cern.ch> 'Fixes for MC dataformat problems.'
    Cherrypick from master 2008-04-29 16:42:04 UTC Simone Gennai <sha1-c06466410f2ad16966d114488822880c0effa70e@cern.ch> 'Restored L25 analyzers':
        src/L25TauAnalyzer.cc
        src/SealModule.cc
    Cherrypick from master 2008-06-03 21:30:44 UTC Rick Wilkinson <sha1-197b93d87bf2e1eb4349df76c6ec25fd8f1f348e@cern.ch> 'update py configs':
        python/L2TauAnalyzer_cfi.py

--- src/SealModule.cc
+++ src/SealModule.cc
@@ -4,8 +4,11 @@
 #include "HLTriggerOffline/Tau/interface/HLTTauMcInfo.h"
 
 #include "HLTriggerOffline/Tau/interface/HLTTauAnalyzer.h"
+#include "HLTriggerOffline/Tau/interface/MCTauCand.h"
+
 #include "HLTriggerOffline/Tau/interface/HLTTauL25Validation.h"
 #include "HLTriggerOffline/Tau/interface/L25TauAnalyzer.h"
+
 #include "HLTriggerOffline/Tau/interface/L2TauAnalyzer.h"
 #include "HLTriggerOffline/Tau/interface/L1TauAnalyzer.h"
 
@@ -17,6 +20,7 @@ DEFINE_ANOTHER_FWK_MODULE(HLTTauL25Validation);
 DEFINE_ANOTHER_FWK_MODULE(L25TauAnalyzer);
 DEFINE_ANOTHER_FWK_MODULE(L2TauAnalyzer);
 DEFINE_ANOTHER_FWK_MODULE(L1TauAnalyzer);
+
 
 
 
