d5793499fcd4e3c2fb347a7fa4db2b78543c25ef
Konstantinos Petridis <sha1-52262e9d1df117cf868d5230eec8da061794b699@cern.ch>
2008-07-09 23:22:19 +0000

    Fixed ETau DQM root File Structure

--- test/ElectronOfETauPlots.C
+++ test/ElectronOfETauPlots.C
@@ -9,8 +9,10 @@
 #include "TH2F.h"
 
 
-void drawall(char* f1, char* f2 ,char* label1, char* label2, char* var , string outname){
+void drawall(char* f1, char* f2 ,string pathName,char* label1, char* label2, char* var , string outname){
  
+  gStyle->SetOptTitle(1);
+  
   TFile* file1 = TFile::Open(f1);
   TFile* file2 = TFile::Open(f2);
 
@@ -21,16 +23,17 @@ void drawall(char* f1, char* f2 ,char* label1, char* label2, char* var , string 
   char* name1=0, *name2=0;
 
   const int npaths = 1;
-  string paths[npaths]={"electronOfETau"};
+  string paths[npaths]={"DQMData/ElectronOf"+pathName+"DQM"};
   string Histo_paths[npaths];
-  for(int i=0;i<npaths;i++){Histo_paths[i]=paths[i]+"DQM/total eff";cout<<Histo_paths[i]<<endl;}
+  for(int i=0;i<npaths;i++){Histo_paths[i]=paths[i]+"/total eff";cout<<Histo_paths[i]<<endl;}
 
   string fname= outname+"(";
   TCanvas* c1= new TCanvas("c1","c1");
   c1->SetFillColor(0);
   c1->Divide(2,2);
 
-  TFile* f=new TFile("ElectronOfETauPlots.root","RECREATE");
+  string out_file=pathName+"Plots.root";
+  TFile* f=new TFile(out_file.c_str(),"RECREATE");
   
   for(int path=0; path< npaths ; path++){
     
@@ -44,8 +47,8 @@ void drawall(char* f1, char* f2 ,char* label1, char* label2, char* var , string 
       name1 = hf1->GetXaxis()->GetBinLabel(filter);
       name2 = hf1->GetXaxis()->GetBinLabel(filter+1);
       
-      string numname = paths[path] + "DQM/"+ name2 + var;
-      string denomname = paths[path] + "DQM/"+ name1 + var;
+      string numname = paths[path] + "/"+ name2 + var;
+      string denomname = paths[path] + "/"+ name1 + var;
       string title = paths[path]+name2+var+"Efficiency";
       
       num1   =  new TH1F( *(TH1F*)file1->Get(numname.c_str()) );
@@ -72,8 +75,9 @@ void drawall(char* f1, char* f2 ,char* label1, char* label2, char* var , string 
       l1 = legend->AddEntry(graph1,label1,"lp");
       l1 = legend->AddEntry(graph2,label2,"lp");
       
-
-      //std::cout << plotnr%4 << std::endl;
+      TPaveText* pave=new TPaveText();
+      pave->AddText(name2);
+      
       if(plotnr%4==0){
 	c1->Clear("D");
       }
@@ -82,6 +86,7 @@ void drawall(char* f1, char* f2 ,char* label1, char* label2, char* var , string 
       graph1->Draw("AP");
       graph2->Draw("Psame");
       legend->Draw();      
+      pave->Draw();
 
       plotnr++;
    
