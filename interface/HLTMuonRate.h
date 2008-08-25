24ca54ea5daaf036037110d89cfd9534ed57cbef
cmsbuild <>
2008-08-25 16:03:59 +0000

    This commit was manufactured by cvs2git to create tag 'HLTriggerOffline- Tau-V02-04-12'.
    Sprout from master 2008-08-25 16:03:58 UTC Michail Bachtis <sha1-eaf6ea55826e5289c86058cfc492a286c076afdf@cern.ch> 'Fixed for Auto-validation'
    Cherrypick from master 2008-08-05 18:40:49 UTC Sho Maruyama <sha1-b47564b910de3cc3cd657a4905f52d93b10eea83@cern.ch> 'first version':
        interface/HLTMuonRate.h
        src/HLTMuonRate.cc

--- interface/HLTMuonRate.h
+++ interface/HLTMuonRate.h
@@ -1,11 +1,13 @@
 #ifndef HLTriggerOffline_Muon_HLTMuonRate_H
 #define HLTriggerOffline_Muon_HLTMuonRate_H
+
 /** \class HLTMuonRate
  *  Get L1/HLT efficiency/rate plots
+ *
  *  \author  Sho Maruyama  (copied from J. Alcaraz
  */
-#include "DQMServices/Core/interface/DQMStore.h"
-#include "DQMServices/Core/interface/MonitorElement.h"
+
+// Base Class Headers
 
 #include "FWCore/Framework/interface/EDAnalyzer.h"
 #include "FWCore/Framework/interface/Frameworkfwd.h"
@@ -14,14 +16,13 @@
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
 #include "FWCore/ParameterSet/interface/InputTag.h"
 #include "DataFormats/Common/interface/RefToBase.h"
+#include "DataFormats/TrackReco/interface/Track.h"
 #include "DataFormats/RecoCandidate/interface/RecoChargedCandidate.h"
 #include "DataFormats/Candidate/interface/Candidate.h"
+#include "SimDataFormats/HepMCProduct/interface/HepMCProduct.h"
 #include <vector>
-
-using namespace std;
-using namespace edm;
-using namespace reco;
-using namespace math;
+#include "DQMServices/Core/interface/DQMStore.h"
+#include "DQMServices/Core/interface/MonitorElement.h"
 
 class HLTMuonRate {
 public:
@@ -32,11 +33,12 @@ public:
   void WriteHistograms() ;
   MonitorElement* BookIt(char name[], char title[], int bins, float min, float max) ;
 private:
-  edm::InputTag theL1CollectionLabel, InputLabel;
+  edm::InputTag theL1CollectionLabel, theGenLabel, theRecoLabel;
   std::vector<edm::InputTag> theHLTCollectionLabels;
   double theL1ReferenceThreshold,theHLTReferenceThreshold;
   std::vector<double> theNSigmas;
   unsigned int theNumberOfObjects;
+  bool useMuonFromGenerator,useMuonFromReco;
   double theCrossSection;
   double theLuminosity;
   double thePtMin;
@@ -46,20 +48,35 @@ private:
   DQMStore* dbe_;  
   MonitorElement* hL1DR, *hL2DR, *hL3DR;
   MonitorElement* hL1eff;
-  MonitorElement* hPtNor;
-  MonitorElement* hPhiNor;
-  MonitorElement* hEtaNor;
+  MonitorElement* hL1MCeff;
+  MonitorElement* hL1RECOeff;
+  MonitorElement* hMCptnor;
+  MonitorElement* hMCphinor;
+  MonitorElement* hMCetanor;
+  MonitorElement* hRECOptnor;
+  MonitorElement* hRECOphinor;
+  MonitorElement* hRECOetanor;
   MonitorElement* hL1rate;
   MonitorElement* hL1pt;
-  MonitorElement* hL1Eta;
-  MonitorElement* hL1Phi;
+  MonitorElement* hL1etaMC;
+  MonitorElement* hL1phiMC;
+  MonitorElement* hL1etaRECO;
+  MonitorElement* hL1phiRECO;
   MonitorElement* hSteps;
   std::vector <MonitorElement*> hHLTeff;
+  std::vector <MonitorElement*> hHLTMCeff;
+  std::vector <MonitorElement*> hHLTRECOeff;
   std::vector <MonitorElement*> hHLTrate;
   std::vector <MonitorElement*> hHLTpt;
-  std::vector <MonitorElement*> hHLTeta;
-  std::vector <MonitorElement*> hHLTphi;
-  std::pair<double,double> getAngle(double eta, double phi, Handle< vector<XYZTLorentzVectorD> >& refVector );
+  std::vector <MonitorElement*> hHLTetaMC;
+  std::vector <MonitorElement*> hHLTphiMC;
+  std::vector <MonitorElement*> hHLTetaRECO;
+  std::vector <MonitorElement*> hHLTphiRECO;
+  HepMC::GenEvent::particle_const_iterator theAssociatedGenPart;
+  reco::TrackCollection::const_iterator theAssociatedRecoPart;
+  const HepMC::GenEvent* evt;
+  std::pair<double,double> getGenAngle(double eta, double phi, HepMC::GenEvent evt, double DR=0.4 );
+  std::pair<double,double> getRecoAngle(double eta, double phi,reco::TrackCollection tracks, double DR=0.4 );
   MonitorElement *NumberOfEvents,*NumberOfL1Events;
   int theNumberOfEvents,theNumberOfL1Events;
   std::string theRootFileName;
